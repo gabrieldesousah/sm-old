e20e560f2c6dfc681abbc518d8f393fc0228eb37
Gabriel Henrique <gabrieldesousa.h@gmail.com>
2016-03-16 02:11:51 +0000

    atualizando o vodeps controller para ter o senf

--- seumerito/app/controllers/videosController.php
+++ seumerito/app/controllers/videosController.php
@@ -8,6 +8,7 @@ class Videos extends Controller{
         
         $this->view('videos', $data);
     }
+    
     public function share(){
 		$posts = new VideosModel();
 
@@ -16,5 +17,42 @@ class Videos extends Controller{
         $this->view('shareVideos', $data);
     }
     
+    public function send(){
+		$table = "videos";
+ 
+	    $sessionHelper = new SessionHelper();
+        
+	    $authCheck = $sessionHelper->checkSession( "userAuth" );
+	    $userData = $sessionHelper->selectSession( "userData" );
+	    if($authCheck === false){
+	        $userData["user_id"] = "";
+	    }
+	        	
+	    $type       = $_POST["type"];
+		$content    = $_POST["content"];
+		$professor  = $_POST["professor"];
+
+		date_default_timezone_set('America/Sao_Paulo');
+   		$date = date('Y-m-d H:i:s', time());
+		        
+		$data = array(
+		    "type" => $type,
+		    "type" => $type,
+		    "type" => $type,
+		    "type" => $type
+		);
+		$db = new Model();	
+			        	
+	    if($db->insert($table,$data)){
+	       	$this->message = "Aula enviada. Muito obrigado e continue compartilhando (:";
+        	echo "<h4>".$this->message."</h4>";
+        	
+        	$posts = new VideosModel();
+
+	        $list_posts = $posts->listPosts(9999);
+	        $data['list_posts'] = $list_posts;
+	        $this->view('shareVideos', $data);
+	    }
+    }
 
 }
